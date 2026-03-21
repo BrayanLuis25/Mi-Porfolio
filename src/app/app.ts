ac8651d37d9e3fc466ab7f4ff676f17a6b77ed02
Brayan Luis <vcbryan19@gmail.com>
2026-03-20 21:29:53 -0500

    primer commit miporfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mi_porfolio');
+
+
 }
+/*ng g c components/navbar
+ng g c components/hero
+ng g c components/profile
+ng g c components/experience
+ng g c components/education
+ng g c components/projects
+ng g c components/skills
+ng g c components/contact
+ng g c components/footer */

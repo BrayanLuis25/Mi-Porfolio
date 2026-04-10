c43f8d9cbf443912bdd52b41d404748e1ad7a68a
Brayan Luis <vcbryan19@gmail.com>
2026-04-10 18:17:49 -0500

    mi porfolio DevL

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+import { Experience } from './components/experience/experience';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
+  imports: [RouterOutlet,Navbar,Home,About
+    ,Skills, Experience, Projects, Contact,Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,8 +23,11 @@ export class App {
 
 
 }
-/*ng g c components/navbar
+/*
 ng g c components/hero
+ng g c components/navbar
+ng g c componentes/home
+
 ng g c components/profile
 ng g c components/experience
 ng g c components/education
--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,11 +1,52 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule, NgClass, RouterModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+cards1 = [
+    {
+      title: 'Proyecto Sistema Hotel Radisson',
+      description: 'Uso de lenguaje java, y Tecnologias: mysql+springboot+angular',
+      icon: 'fa-solid fa-hotel',
+      color: 'text-danger',
+      link: 'https://github.com/BrayanLuis25/springboot-angular-bd',
+      button: 'Ver codigo de mi sistema hotel'
+    },
+    {
+      title: 'Sistema-de-Gestion-Veterinaria-Crocketas-Life-en-IOS',
+      description: 'Uso de lenguaje swift y IDE XCODE',
+      icon: 'fa-solid fa-shield-dog',
+      color: 'text-warning',
+      link: 'https://github.com/BrayanLuis25/Sistema-de-Gesti-n-Veterinaria-Crocketas-Life-en-IOS.git',
+      button: 'Ver codigo de mi sistema veterinaria Crocketas'
+    /*<i class="fa-solid fa-shield-dog" style="color: rgb(205, 170, 23);"></i> */
+    },
+    {
+      title: 'App-Movil-para-la-Gestion-Integral-de-Eventos-Infantiles-en-Happyland',
+      description: 'Uso de lenguaje kotlin y IDE Android Studio',
+      icon: 'fa-solid fa-children',
+      color: 'text-success',
+      link: 'https://github.com/BrayanLuis25/App-Movil-para-la-Gestion-Integral-de-Eventos-Infantiles-en-Happyland.git',
+      button: 'Ver codigo de mi sistema de eventos infantiles'
+    },
+    /*<i class="fa-solid fa-children" style="color: rgb(23, 222, 17);"></i> */
+    {
+      title: 'SPA VETERINARIA HUELLITAS',
+      description: 'spa formulario uso de lenguaje jsx y framework React',
+      icon: 'fa-solid fa-shield-cat',
+      color: 'text-info',
+      link: 'https://brayanluis25.github.io/react-example1/',
+      button: 'Ver codigo de mi Single Page Application '
+    }
+    /*<i class="fa-solid fa-shield-cat" style="color: rgb(99, 230, 190);"></i> */
+    
+  ];
 
 }
